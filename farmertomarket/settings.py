2217b94b52d43f0d87ff04331c2ba9fa5899faa1
edingha <ghanta.dinesh@gmail.com>
2021-08-12 20:18:07 +0530

    Allowed hosts

--- farmertomarket/settings.py
+++ farmertomarket/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool) # True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'django-cart-env.eba-phmjmy92.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
