272694c6529acd2247a3833f15bb45fe8dc1c433
edingha <ghanta.dinesh@gmail.com>
2021-08-12 22:31:46 +0530

    AWS Update

--- farmertomarket/settings.py
+++ farmertomarket/settings.py
@@ -90,6 +90,7 @@ AUTH_USER_MODEL = 'accounts.Account'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 # Database Configuration
+
 if 'RDS_DB_NAME' in os.environ:
     DATABASES = {
         'default': {
@@ -146,14 +147,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+'''
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR /'static'
 STATICFILES_DIRS = [
      'farmertomarket/static',
- ]
+ ]'''
 
-'''# AWS S3 Static Files Configuration
+# AWS S3 Static Files Configuration
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
@@ -166,13 +167,13 @@ AWS_DEFAULT_ACL = 'public-read'
 AWS_LOCATION = 'static'
 
 STATICFILES_DIRS = [
-    'greatkart/static',
+    'farmertomarket/static',
 ]
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-DEFAULT_FILE_STORAGE = 'greatkart.media_storages.MediaStorage'
-'''
+DEFAULT_FILE_STORAGE = 'farmertomarket.media_storages.MediaStorage'
+
 
 
 # media files configuration
